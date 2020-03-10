7fd2b37a1a8fabce2503f5e58aecf0357388bc80
Eric Xu <xujinzheng0610@gmail.com>
2020-03-11 00:52:51 +0800

    initial setup

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pymongo import MongoClient
+
+app = Flask(__name__)
+title = "TransACT Server"
+CORS(app)
+
+client = MongoClient("mongodb+srv://transactAdmin:transact@transact-tsjmg.mongodb.net/test?retryWrites=true&w=majority")
+db = client['transact']
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/test", methods=['GET'])
+def testGet():
+    dic = {"value": 1}
+    return jsonify(dic)
+
+@app.route("/registerDonor", methods=['POST'])
+def rigisterDonor():
+    email = request.form["email"]
+    password = request.form["password"]
+    # TODO: complete rigiester donor code
+
+    return jsonify(200)
+
+if __name__ == "__main__":
+    app.run(debug=True)
