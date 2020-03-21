b1d806c5fb7f998b52076ce058e6caf4e57afb73
Eric Xu <xujinzheng0610@gmail.com>
2020-03-21 21:52:57 +0800

    add db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 title = "TransACT Server"
@@ -18,13 +19,33 @@ def testGet():
     dic = {"value": 1}
     return jsonify(dic)
 
-@app.route("/registerDonor", methods=['POST'])
-def rigisterDonor():
-    email = request.form["email"]
-    password = request.form["password"]
-    # TODO: complete rigiester donor code
+@app.route("/charity/register", methods=['POST'])
+def registerDonor():
+    charity = db.charity
+    try:
+        charity.insert_one({
+            "email":request.form["email"], 
+            "password":request.form["password"],
+            "username": request.form["username"],
+            "region": request.form["region"]
+        })
+        
+    except e:
+        print(e)
 
     return jsonify(200)
+
+@app.route("/donor/login", methods=['GET'])
+def loginDonor():
+    donors = db.donors
+    try:
+        results = donors.find_one({"_id": ObjectId("5e7611d5d594a75088cec8d1")})
+        results["_id"] = str(results["_id"])
+        print(results)
+    except:
+        print("error")
+
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
